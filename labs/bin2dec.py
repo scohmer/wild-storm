91f1e11e6d7280fe444a994e44e315cb8983fb96
mpxf <diamond061@outlook.com>
2022-09-26 09:42:32 -0400

    Finalized lab class / function structure

--- labs/bin2dec.py
+++ labs/bin2dec.py
@@ -6,4 +6,18 @@ class Bin2Dec(object):
         self.author = "Sean Cohmer, Cooper Violette"
 
     def lab(self):
-        print(NumberLabCore.main(self, "bin", "dec"))
+        # Initialize flag output check
+        flagData = []
+
+        # Begin user input validation
+        while len(flagData) < 3:
+            num = NumberLabCore.main(self, "bin", "dec")
+            if (userInput := input(f"Convert {num[0]} to decimal: ")) == str(num[1]):
+                flagData.append(int(userInput))
+            else:
+                print("Try again")
+
+        # Return flag
+        return print(f"Your flag is: {hex(sum(flagData))}")
+        
+
